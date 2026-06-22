d99e080fb8912f8885f343b53be10ec779daf6a3
Rachit Raj <rachitraj@rachits-air.home>
2026-06-22 07:40:48 -0400

    Remove ACTO branding

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/acto-superagent/',
+  base: '/-superagent/',
   plugins: [react()],
   server: {
     port: 5173,
